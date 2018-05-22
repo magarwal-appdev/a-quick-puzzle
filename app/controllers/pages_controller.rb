6d1c0db01c5f70810125c8ac21d95a9455366888
Raghu Betina <raghu@firstdraft.com>
2018-05-22 09:41:54 -0500

    Connect RCAV dots

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,6 @@
+class PagesController < ApplicationController
+  def puzzle
+
+    render("pages/puzzle.html.erb")
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get("/a-quick-puzzle", { :controller => "pages", :action => "puzzle" })
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
